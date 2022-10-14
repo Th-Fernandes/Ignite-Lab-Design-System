fd4dbb82877a89445abf91fbff4ad468a2871239
Thiago Fernandes <tjfctigo@gmail.com>
2022-10-14 06:46:29 -0300

    signIn Page

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,67 @@
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { Logo } from "../components/Logo";
+import { Text } from "../components/Text";
+import { TextInput } from "../components/TextInput";
+import { Envelope, Lock } from "phosphor-react";
+
+export function SignIn() {
+  return (
+    <div className="w-screen h-screen bg-gray-900 items-center justify-center flex">
+      <div className="w-full max-w-[400px]">
+        <header className="flex flex-col items-center">
+          <Logo />
+          <Heading size="lg" className="mt-2">ignite lab</Heading>
+          <Text size="lg" className="text-gray-400 mt-1">Faça login e comece a usar!</Text>
+        </header>
+
+        <form className="flex flex-col gap-4 mt-10 ">
+          <div>
+            <label htmlFor="email" >
+              <Text className="text-sm font-semibold mb-4 block">Endereço de E-mail</Text>
+            </label>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Envelope />
+              </TextInput.Icon>
+              <TextInput.Input id="email" placeholder="exemplo@gmail.com" />
+            </TextInput.Root>
+          </div>
+
+          <div>
+            <label htmlFor="password" >
+              <Text className="text-sm font-semibold mb-4 block">Sua senha</Text>
+            </label>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Lock />
+              </TextInput.Icon>
+              <TextInput.Input id="password" type="password" placeholder="********" />
+            </TextInput.Root>
+          </div>
+
+          <label htmlFor="remember" className="flex items-center gap-2">
+            <Checkbox id="remember" />
+            <Text size="sm" className="text-gray-200">Lembrar-se de mim por 30 dias</Text>
+          </label>
+
+          <Button type="submit" className="mt-4">
+            Entrar na plataforma
+          </Button>
+        </form>
+
+        <footer className="flex flex-col gap-4 items-center mt-8">
+          <a className="text-gray-400 underline hover:text-gray-200 transition-colors text-xs" href="#">
+            Esqueceu sua senha?
+          </a>
+
+          <a className="text-gray-400 underline hover:text-gray-200 transition-colors text-xs" href="#">
+            Não possui conta? Crie uma agora!
+          </a>
+
+        </footer>
+      </div>
+    </div>
+  )
+}
